09bd4cb70ee13cbebb77af749a091abdc6398041
dmytro <dzeleniy9@gmail.com>
2020-11-02 17:59:43 +0200

    Initial commit

---
+++ app/api/api_weather_forecast.py
@@ -0,0 +1,12 @@
+# Get weathe forecasts from these APIs
+
+# OpenWeatherMap API
+# Weatherbit API
+# AccuWeather API
+# Dark Sky API
+# Weather2020 API
+# ClimaCell API
+# Visual Crossing API
+
+# class WeatherForecast:
+#
